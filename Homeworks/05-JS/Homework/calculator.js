4fcf919743354dca3a10b28a79ee270888c70ffb
Dmitrijs Cernevics <draimers@mail.ru>
2022-03-21 19:54:56 +0100

    Calculator created, directory with name and surname created, calculation added

--- Homeworks/05-JS/Homework/calculator.js
+++ Homeworks/05-JS/Homework/calculator.js
@@ -1,8 +1,8 @@
 /* Создать калькулятор
-    создать директорию с ИмяФамилия создать index.html и подключить туда этот скрип
+    + создать директорию с ИмяФамилия создать index.html и подключить туда этот скрип
     
     Дополнить скрипт
-    - добавить сложение, вычитание, умножение, деление
+    + добавить сложение, вычитание, умножение, деление
     - вывести alert() при делении на ноль
     - проверить типы 
     - сделать преобразование типов
@@ -19,7 +19,18 @@ const number2 = prompt('Второе число: ');
 let result;
 
 // using if...else if... else
-
-
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+    }
+else if (operator == '*') {
+    result = number1 * number2;
+        }
+else if (operator == '/') {
+    result = number1 / number2;
+            }
 // display the result
 console.log(`${number1} ${operator} ${number2} = ${result}`);
+window.alert(`${number1} ${operator} ${number2} = ${result}`);
