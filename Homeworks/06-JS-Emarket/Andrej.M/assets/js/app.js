bd395f21507cc2c2988c91fb165624b38df4c565
Andrej.M <studija.www@gmail.com>
2022-04-02 20:44:03 +0300

    id-class

--- Homeworks/06-JS-Emarket/Andrej.M/assets/js/app.js
+++ Homeworks/06-JS-Emarket/Andrej.M/assets/js/app.js
@@ -9,9 +9,9 @@ function renderProducts(data) {
                   <img src="${element.imageURL}" alt="">
                </div>
                <div class="prod-data" >
-                  <span id="prod-title">${element.name}</span>
-                  <span id="prod-price">$ ${element.price}</span>
-                  <p id="prod-description">${element.description}</p>
+                  <span class="prod-title">${element.name}</span>
+                  <span class="prod-price">$ ${element.price}</span>
+                  <p class="prod-description">${element.description}</p>
                   <form action="javascript:addToCart('${element.id}')" class="cart-btn">
                      <button class="btn" id="add-to-cart" type="submit">Заказать</button>
                   </form>
