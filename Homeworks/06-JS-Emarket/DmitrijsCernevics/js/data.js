feb78d9cf727960f8032fa4d6ac53e84b71809ba
Dmitrijs Cernevics <draimers@mail.ru>
2022-04-04 16:56:28 +0200

    Last change for Lesson 7

--- Homeworks/06-JS-Emarket/DmitrijsCernevics/js/data.js
+++ Homeworks/06-JS-Emarket/DmitrijsCernevics/js/data.js
@@ -1,7 +1,5 @@
-const data = [
-
-	// Пицы
-    {
+[
+	{
 		"id": "Pizza1",
 		"name": "Аррива",
 		"price": "10",
@@ -12,7 +10,7 @@ const data = [
 	{
 		"id": "Pizza2",
 		"name": "Гавайская",
-		"price": "9",
+		"price": "10",
 		"description": "30 см, 650 г, Ветчина, ананасы, моцарелла, томатный соус",
 		"imageURL": "./images/Гавайская.jpeg",
 		"category": "pizza"
@@ -28,7 +26,7 @@ const data = [
 	{
 		"id": "Pizza4",
 		"name": "Додо",
-		"price": "11",
+		"price": "10",
 		"description": "30 см, 740 г, Бекон, митболы, пикантная пепперони, моцарелла, томаты, шампиньоны, сладкий перец, красный лук, чеснок, томатный соус",
 		"imageURL": "./images/Додо.jpeg",
 		"category": "pizza"
@@ -36,17 +34,15 @@ const data = [
 	{
 		"id": "Pizza5",
 		"name": "Пепперони",
-		"price": "7",
+		"price": "10",
 		"description": "30 см, 580г, Пикантная пепперони, увеличенная порция моцареллы, томатный соус",
 		"imageURL": "./images/Пепперони.jpeg",
 		"category": "pizza"
 	},
-
-	// Напитки
 	{
 		"id": "Drink1",
 		"name": "Вода",
-		"price": "1.5",
+		"price": "10",
 		"description": "BonAqua, негазированная, 0,5 л",
 		"imageURL": "./images/Вода.jpeg",
 		"category": "pizza"
@@ -54,7 +50,7 @@ const data = [
 	{
 		"id": "Drink2",
 		"name": "Кола",
-		"price": "2",
+		"price": "10",
 		"description": "Газированный напиток Coca-cola, 0.5л",
 		"imageURL": "./images/Кола.jpeg",
 		"category": "pizza"
@@ -62,7 +58,7 @@ const data = [
 	{
 		"id": "Drink3",
 		"name": "Спрайт",
-		"price": "2",
+		"price": "10",
 		"description": "Газированный напиток Sprite, 0.5л",
 		"imageURL": "./images/Спрайт.jpeg",
 		"category": "pizza"
@@ -70,7 +66,7 @@ const data = [
 	{
 		"id": "Drink4",
 		"name": "Фанта",
-		"price": "2",
+		"price": "10",
 		"description": "Газированный напиток Fanta, 0.5л",
 		"imageURL": "./images/Фанта.jpeg",
 		"category": "pizza"
@@ -78,34 +74,9 @@ const data = [
 	{
 		"id": "Drink5",
 		"name": "Чай",
-		"price": "1.5",
+		"price": "10",
 		"description": "FuzeTea Черный с лимоном и лемонграссом, 0,5 л",
 		"imageURL": "./images/Чай.jpeg",
 		"category": "pizza"
 	}
 ]
-window.onload = renderProduct(data); // Команда которая вызывает функцию
-function renderProduct (data) { //создаём функцию и указываем путь откуда берём данные и её можно будет вызвать
-    let productContainer = document.getElementById('product-container'); //задаём переменную с путём куда именно вставлять данные
-    productContainer.innerHTML = ""; //что именно передаём
-    data.forEach(element => { //из данных получаем определённый элемент
-        productContainer.innerHTML += `
-        <div class="product-item">
-            <div class="prod-image">
-                <img src="${element.imageURL}" alt="">
-            </div>
-            <div class="prod-data">
-                <span id="prod-title">${element.name}</span>
-                <span id="prod-price">€ ${element.price}</span>
-                <span id="prod-price">${element.description}</span>
-                <form action="javascript:addToCart('${element.id}')" class="cart-btn">
-				<button class="btn" id="add-to-cart" type="submit">Заказать</button>
-			</form>
-            </div>
-        </div>
-        ` //создаём элемент из данных data в блоке <div>, ести просто =, то каждый элемент будет перезаписывать следующий элемент. Поэтому даобавляется +.
-    });
-}
-
-
-// debugger;
