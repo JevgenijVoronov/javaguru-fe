92bac8a547f8fb43cbd76b0eb1dc8d5935f12bce
Andrej.M <studija.www@gmail.com>
2022-04-05 20:19:14 +0300

    калькулятор - обновил код (нет on/off, %, +.-,)

--- Homeworks/06-JS-Emarket/Andrej.M/calc/main.js
+++ Homeworks/06-JS-Emarket/Andrej.M/calc/main.js
@@ -1,24 +1,55 @@
-const calc = document.querySelector(".buttons"); //обращаемся к .buttons
-const result = document.querySelector(".result"); //экран
+const display = document.querySelector(".result")     //экран
+//const buttons = document.querySelector(".container");    //обращаемся к .buttons (предыдущий код)
 
-calc.addEventListener("click", function (event) {   //вешаем функцию click
+function insert(num) {
+  display.textContent = display.textContent + num     // вывод значения кнопок
 
-  if (!event.target.classList.contains("btn")) return; //проверка на .btn
+}
 
-  const value = event.target.innerText; //извлекаем текст кнопки
+function reset() {
+  display.textContent = ""                            // сброс
+}
 
+function back() {                                    // сброс на 1 символ
+  let exp = display.textContent
+  display.textContent = exp.substring(0, exp.length - 1)
+}
 
-  switch (value) {
-    case "C":
-      result.innerText = ""; //кнопка сброса
-      break;
-
-    case "=":
-      if (result.innerText.search(/[^0-9*/+-.)(]/im) != -1) return; //eval - проверка безопастности
-      result.innerText = eval(result.innerText).toFixed(2);       // кнопка =
-      break;
-
-    default:
-      result.innerText += value; //выводим значения на экран
+function equals() {                                 // равно
+  let exp = display.textContent
+  if (exp) {
+    display.textContent = eval(exp)
   }
-});
+}
+// function percent(){
+//   display.innerText = eval(display.innerText)/100    //проценты
+//}
+
+
+//---------------------------// Этот код тормозил!
+
+// buttons.addEventListener("click", function (event) {   //вешаем функцию click
+
+//    if (!event.target.classList.contains("btn")) return;   //проверка на .btn
+
+//   const btnText = event.target.innerText;               //извлекаем текст кнопки
+
+
+//   switch (btnText) {
+//     // case "C":
+//     //   display.innerText = "";                                //кнопка сброса
+//     //   break;
+
+//       case "Del":
+//       display.innerText = display.innerText.slice(0, -1);
+//       break;
+
+//     case "=":
+//        // if (result.innerText.search(/[^0-9*/+-.)(]/im) != -1) return;   //eval - проверка безопастности
+//       display.innerText = eval(display.innerText).toFixed(2);       // кнопка =
+//       break;
+
+//     default:
+//       display.innerText += btnText;                          //выводим значения на экран
+//   }
+// });
