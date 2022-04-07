424c20940c58bd372803232bb41072e23476eed6
ylyuday <ylyuday@gmail.com>
2022-04-07 23:51:50 +0300

    LiudmilYunevich-06-JS-Emarket

---
+++ Homeworks/06-JS-Emarket/liudmiyunevich/app.js
@@ -0,0 +1,30 @@
+// render products based on given categories
+function renderProducts(data) {
+	let productContainer = document.getElementById('product-container');
+	productContainer.innerHTML = "";
+	data.forEach(item => {
+      productContainer.innerHTML +=` 
+            <div class="product-item">
+               <div class="prod-image">
+                  <img src="${item.imageURL}" alt="">
+               </div>
+               <div class="prod-data" >
+                  <span id="prod-title">${item.name}</span>
+                  <span id="prod-price">$ ${item.price}</span>
+                  <p id="prod-description">${item.description}</p>
+                  <form action="javascript:addToCart('${item.id}')" class="cart-btn">
+                     <button class="btn" id="add-to-cart" type="submit">Заказать</button>
+                  </form>
+               </div>
+            </div>
+            <div class="vertical-space"></div>
+      `
+		}
+	
+   );
+}
+document.addEventListener("DOMContentLoaded",renderProducts(data));
+
+// window.addEventListener("keydown", function (event) {
+//    alert("Вы нажали на кнопку: " + event.key)
+//  })
---
+++ Homeworks/06-JS-Emarket/liudmiyunevich/data.js
@@ -0,0 +1,84 @@
+const data = [
+	{
+        "id": "pizza_1",
+		"name": "Маргарита",
+		"price": "10",
+		"description": "Томатная паста,чеснок,базилик,моцарелла,помидоры черри",
+		"imageURL": "https://thumb.tildacdn.com/tild3630-3739-4630-b135-636564326335/-/resize/275x/-/format/webp/Margarita-removebg-p.png" ,
+		"category": "pizza"
+	},
+
+    {
+        "id": "pizza_2",
+		"name": "Карбонара",
+		"price": "12",
+		"description": "Томатный соус, моцарелла, пармезан, яйца, бекон",
+		"imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrUEDVImZA6m0jIGU46trdsmroOkNJrgAVVQ&usqp=CAU",
+		"category": "pizza"
+	},
+
+    {
+        "id": "pizza_3",
+		"name": "Четыре сыра",
+		"price": "15",
+		"description": "Томатный соус, моцарелла, пармезан, сыр горгонзола, артишоки и орегано",
+		"imageURL": "https://foxpizza.by/upload/iblock/35b/35b2c472c8922d1a0b3e46f8403ef79f.png",
+		"category": "pizza"
+	},
+    {
+        "id": "pizza_4",
+		"name": "Неаполетано",
+		"price": "18",
+		"description": "Томатный соус, моцарелла, орегано, анчоусы",
+		"imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRYMdBJjuqSer-mZ0L2ot0n4uFIONBPjDAFw&usqp=CAU",
+		"category": "pizza"
+	},
+    {
+        "id": "pizza_5",
+		"name": "Крудо",
+		"price": "19",
+		"description": "Томатный соус, моцарелла и пармская ветчина",
+		"imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwTYQ-9yAlJlgQv72nmkIC1Ok2jcLTlLctng&usqp=CAU",
+		"category": "pizza"
+	},
+    {
+        "id": "drink_1",
+		"name": "Пепси",
+		"price": "2",
+		"description": "газированный безалкогольный напиток",
+		"imageURL": "https://calorizator.ru/sites/default/files/imagecache/product_512/product/pepsi.jpg",
+		"category": "drinks"
+	},
+    {
+        "id": "drink_2",
+		"name": "Кока-кола",
+		"price": "1.5",
+		"description": "газированный безалкогольный напиток",
+		"imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNMUUrBick0e12BhZCXepfOotELQxrNeVUKA&usqp=CAU",
+		"category": "drinks"
+	},
+    {
+        "id": "drink_3",
+		"name": "Фанта",
+		"price": "2",
+		"description": "газированный безалкогольный напиток",
+		"imageURL": "https://s3.ibta.ru/goods/620872/016e5d78d6e657435297acadbfc01dd2_xl.jpg",
+		"category": "drinks"
+	},
+    {
+        "id": "drink_4",
+		"name": "Миринда",
+		"price": "1.5",
+		"description": "газированный безалкогольный напиток",
+		"imageURL": "https://calorizator.ru/sites/default/files/imagecache/product_512/product/mirinda.jpg",
+		"category": "drinks"
+	},
+    {
+        "id": "drink_5",
+		"name": "Спрайт",
+		"price": "1.3",
+		"description": "газированный безалкогольный напиток",
+		"imageURL": "https://calorizator.ru/sites/default/files/imagecache/product_512/product/sprite.jpg",
+		"category": "drinks"
+	}
+]
