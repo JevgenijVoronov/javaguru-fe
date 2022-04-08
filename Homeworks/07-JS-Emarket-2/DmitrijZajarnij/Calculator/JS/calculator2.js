fda33ee7c9d2bf3059305d792b542e16dc1daadb
Dmitrij Zajarnij <zajarnij89@inbox.lv>
2022-04-08 13:58:21 +0300

    trying make function on my calculator ((((,but also again false!!!!

---
+++ Homeworks/07-JS-Emarket-2/DmitrijZajarnij/Calculator/JS/calculator2.js
@@ -0,0 +1,105 @@
+ce();
+one();
+two();
+three();
+four();
+five();
+six();
+seven();
+eight();
+nine();
+zero();
+plus();
+minus();
+multiply();
+divide();
+equal();
+
+const out = document.querySelector('.screen p');
+
+function ce() {
+    if(document.numbers === ce)
+    return;
+}
+
+function one() {
+    if(document.numbers === one)
+    out.textContent = one;
+}
+
+function two() {
+    if(document.numbers === two)
+    return;
+}
+
+function three() {
+    if(document.numbers === three)
+    return;
+}
+
+function one() {
+    if(document.numbers === one)
+    return;
+}
+
+function four() {
+    if(document.numbers === four)
+    return;
+}
+
+function five() {
+    if(document.numbers === five)
+    return;
+}
+
+function six() {
+    if(document.numbers === six)
+    return;
+}
+
+function seven() {
+    if(document.numbers === seven)
+    return;
+}
+
+function eight() {
+    if(document.numbers === eight)
+    return;
+}
+
+function nine() {
+    if(document.numbers === nine)
+    return;
+}
+
+function zero() {
+    if(document.numbers === zero)
+    return;
+}
+
+function plus() {
+    if(document.numbers === plus)
+    return;
+}
+
+function minus() {
+    if(document.numbers === minus)
+    return;
+}
+
+function multiply() {
+    if(document.numbers === multiply)
+    return;
+}
+
+function divide() {
+    if(document.numbers === divide)
+    return;
+}
+
+function equal() {
+    if(document.numbers === equal)
+    return;
+}
+
+window.onload = document.querySelector('numbers');
